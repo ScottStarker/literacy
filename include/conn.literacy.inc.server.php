ef4461e207c4713bfe6c7703182a7083ed999839
ScottStarker <Scott_Starker@sil.org>
2025-04-05 10:24:32 -0700

    Update conn.literacy.inc.server.php
    fix

--- include/conn.literacy.inc.server.php
+++ include/conn.literacy.inc.server.php
@@ -3,23 +3,11 @@ function get_my_db() {
     static $db;
 
     if (!$db) {
-/* 
-	In Windows 7 machine under Apache:
-		In DOS, cd \xampplite\xampplite\mysql\bin
-		and type "mysql --user=root"
-		at "mysql> "
-		type "GRANT ALL ON ilvmeico_literacy.* TO 'root'@'localhost' IDENTIFIED BY 'TtQi3QUjtGimeqK';" // password here
-		at "mysql> "
-		type "quit"
-		Just execute this 1 time for each machine and your good to go!
-*/
 	//set the database connection variables
 	// The OOP is different than the procedual code. The port needs to be seperate from $dbHost and put in mysqli (2222).
-	// localhost needs to be 127.0.0.1 for php 5.3+!
-	// the next 2 lines are for the local computers
 	$dbHost = 'localhost';
 	$dbUser = 'root';
-	$dbPass = 'TtQi3QUjtGimeqK';					// password here
+	$dbPass = 'set server password here';					// password here
 	$dbDatabase = 'ilvmeico_literacy';
 	
 	//connect to the database 
@@ -30,8 +18,7 @@ function get_my_db() {
 	
 	/* change character set to utf8 */
 	$db->set_charset("utf8");
-	// $db->query("SET NAMES 'utf8'"); 		// is not recommended (MySQL 5.3+)
-	}
-	return $db;
+     }
+     return $db;
 }
 ?>
